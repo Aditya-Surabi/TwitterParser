f5f323b224cd282117b4d3daae04ba090a55d2c3
Aditya-Surabi <adityasurabi@gmail.com>
2016-08-11 21:13:44 -0400

    removed api credentials

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
-  consumer_key:         'brmQRTYtYLyVibkEbjmfbIuuJ',
-  consumer_secret:      'mOXCQTFsVZ9Jdz35UEwtMrQZup9hcLe0ddE5qSn8jGMFUYwM5K',
-  access_token:         '763816516093612034-ovASdcCBSrg2TXuCAjev31mJ20mBJTW',
-  access_token_secret:  'HZS0eMD0keEFyPMj5ZaFKyu1nTynd34z6wpQQQ7X3FioW',
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
   //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 
 }
